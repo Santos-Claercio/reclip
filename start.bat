78c3dfe2560f89ee5222a29517543061c5a9d10a
Santos-Claercio <jsantosclaercio@gmail.com>
2026-06-30 15:16:37 -0300

    configuração para rodar com Python 3.14

--- start.bat
+++ start.bat
@@ -1,6 +1,7 @@
 @echo off
 cd /d "%~dp0"
-start "" /B python app.py
+py -3.14 -m pip install -q -r requirements.txt 2>nul
+start "" /B py -3.14 app.py
 echo App iniciada em http://127.0.0.1:8899
 timeout /t 2 >nul
 start http://127.0.0.1:8899
